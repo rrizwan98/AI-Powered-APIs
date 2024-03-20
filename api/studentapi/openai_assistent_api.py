4195ff8780e374a2397937e641909ad1ff631c9f
Raza Rizwan <razarizwanahmed545@gmail.com>
2024-03-20 20:40:08 +0500

    deploy student api on aws app runner

--- api/studentapi/openai_assistent_api.py
+++ api/studentapi/openai_assistent_api.py
@@ -17,13 +17,14 @@ import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# API_KEY = os.getenv("OPENAI_API_KEY")
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 # print(OPENAI_API_KEY)
 
 # print("read_all_students",read_all_students())             #pass the image url
 
-client : OpenAI = OpenAI(api_key=OPENAI_API_KEY)
-
+API_KEY = os.environ.get('OPENAI_API_KEY')
+client = OpenAI(api_key=API_KEY)
 # Map available functions
 available_functions = {
     "read_all_students_data": read_all_students,
@@ -196,7 +197,6 @@ def studentAssistant(prompt: str):
 
     return response
     
-# response1 = fmp_financial_analyst("""
+# response1 = studentAssistant("""
 # list the all student in db
 # """)
-# pretty_print(response1)
